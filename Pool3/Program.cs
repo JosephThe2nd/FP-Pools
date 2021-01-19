23b0af36d9c934080766bd1f5e5df33fcb835bd7
Covasan Iosif <covasan.iosif7@gmail.com>
2021-01-19 21:27:00 +0200

    Began

---
+++ Pool3/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pool3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Problema1();
+            Problema2();
+            Problema3();
+            Problema4();
+            Problema5();
+            Problema6();
+            Problema7();
+            Problema8();
+            Problema9();
+            Problema10();
+            Problema11();
+            Problema12();
+            Problema13();
+            Problema14();
+            Problema15();
+            Problema16();
+            Problema17();
+            Problema18();
+            Problema19();
+            Problema20();
+            Problema21();
+            Problema22();
+            Problema23();
+            Problema24();
+            Problema25();
+            Problema26();
+            Problema27();
+            Problema28();
+            Problema29();
+            Problema30();
+            Problema31();
+
+
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        private static void Problema1()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
